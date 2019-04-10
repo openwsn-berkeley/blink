d597fd40459f5009e42364d41c7a858e3304469b
ba-haile <haivsgd@gmail.com>
2019-04-10 18:56:39 +0200

    Adding latest SmartmeshIp SDK

--- app/MgrBlinkData/MgrBlinkData.py
+++ app/MgrBlinkData/MgrBlinkData.py
@@ -14,6 +14,8 @@ if __name__ == "__main__":
 #============================ imports =========================================
 
 import traceback
+import json
+import datetime
 
 from SmartMeshSDK                       import sdk_version
 from SmartMeshSDK.IpMgrConnectorSerial  import IpMgrConnectorSerial
@@ -25,15 +27,31 @@ from SmartMeshSDK.protocols.blink       import blink
 def handle_data(notifName, notifParams):
     mac_address = '-'.join(['%02x'%b for b in notifParams.macAddress])
     payload     = ''.join([chr(b) for b in notifParams.data])
+    listRSSI = []
+    listNeighbor = []
     try: 
         data, neighbors = blink.decode_blink(payload)
         if data:
             print 'Blink packet received from {0}'.format(mac_address)
             for neighbor_id, rssi in neighbors:
                 print '    --> Neighbor ID = {0},  RSSI = {1}'.format(neighbor_id, rssi)
+                listRSSI.append(rssi)
+                listNeighbor.append(neighbor_id)                
+            if len(listNeighbor) == 1:
+                jsonRssi = {'Time': '{}'.format(datetime.datetime.now()), 'Payload':'{}'.format(data),'BlinkAddress': '{0}'.format(mac_address), 'HeardMote': [{'MoteID': '{0}'.format(listNeighbor[0]), 'RSSI': '{0}'.format(listRSSI[0])}]}
+            elif len(listNeighbor) == 2:
+                jsonRssi = {'Time': '{}'.format(datetime.datetime.now()), 'Payload':'{}'.format(data),'BlinkAddress': '{0}'.format(mac_address), 'HeardMote': [{'MoteID': '{0}'.format(listNeighbor[0]), 'RSSI': '{0}'.format(listRSSI[0])},{'MoteID': '{0}'.format(listNeighbor[1]), 'RSSI': '{0}'.format(listRSSI[1])}]}
+            elif len(listNeighbor) == 3:
+                jsonRssi = {'Time': '{}'.format(datetime.datetime.now()), 'Payload':'{}'.format(data),'BlinkAddress': '{0}'.format(mac_address), 'HeardMote': [{'MoteID': '{0}'.format(listNeighbor[0]), 'RSSI': '{0}'.format(listRSSI[0])},{'MoteID': '{0}'.format(listNeighbor[1]), 'RSSI': '{0}'.format(listRSSI[1])},{'MoteID': '{0}'.format(listNeighbor[2]), 'RSSI': '{0}'.format(listRSSI[2])}]}
+            else:
+                jsonRssi = {'Time': '{}'.format(datetime.datetime.now()), 'Payload':'{}'.format(data),'BlinkAddress': '{0}'.format(mac_address), 'HeardMote': [{'MoteID': '{0}'.format(listNeighbor[0]), 'RSSI': '{0}'.format(listRSSI[0])},{'MoteID': '{0}'.format(listNeighbor[1]), 'RSSI': '{0}'.format(listRSSI[1])},{'MoteID': '{0}'.format(listNeighbor[2]), 'RSSI': '{0}'.format(listRSSI[2])},{'MoteID': '{0}'.format(listNeighbor[3]), 'RSSI': '{0}'.format(listRSSI[3])}]}
+            with open('RSSIvalue.json', 'a') as f:                    
+                f.write(json.dumps(jsonRssi))
+                f.write('\n')                                                    
             if not neighbors:
                 print '    --> Neighbors = n/a'            
             print '    --> Data Sent = {0}\n\n'.format(data.encode("hex"))
+            
     except:
         # handle non-blink data
         pass
@@ -82,4 +100,6 @@ except:
     print 'Script ended with an error.'
     
 raw_input('Press Enter to close.')
+
+
 
